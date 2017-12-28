316b74e241dff7a5c08447455e0979875661f66f
Jakub Hladik <jhladik@iastate.edu>
2017-12-28 14:37:51 +0100

    Initial Commit

---
+++ GameEngine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameEngine
+//
+//  Created by Jakub Hladik on 12/28/17.
+//  Copyright © 2017 Jakub Hladik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
