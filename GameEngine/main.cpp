751c293df40103818467a676d951e2fd2c847ac6
Jakub Hladik <jhladik@iastate.edu>
2017-12-28 18:29:49 +0100

    Changed to GLAD loader instead of GLEW.

--- GameEngine/main.cpp
+++ GameEngine/main.cpp
@@ -7,21 +7,21 @@
 //
 #include <iostream>
 
-#include <GL/glew.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-// keyboard input callback
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+// input handling function
+void processInput(GLFWwindow *window)
 {
-    // if escape is pressed, exit the application
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+    // if escape is pressed, close the application
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
 int main(void)
 {
     // initialize glfw
-    if (glfwInit() != GLFW_TRUE)
+    if (!glfwInit())
     {
         std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
@@ -30,8 +30,10 @@ int main(void)
     // set up opengl core 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     
     // create a window
@@ -48,32 +50,29 @@ int main(void)
     glfwMakeContextCurrent(window);
     
     // initialize glew
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        std::cout << "Failed to initialize GLEW" << std::endl;
-        glfwTerminate();
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
-    // set keyboard input callback
-    glfwSetKeyCallback(window, key_callback);
     
     // set up viewport size
     glViewport(0, 0, 800, 600);
     
-    // clear the buffer
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    
     // main game loop
     while(!glfwWindowShouldClose(window))
     {
-        // check all input events
-        glfwPollEvents();
+        // handle input
+        processInput(window);
+
         
         // render
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         
         // swap buffers
         glfwSwapBuffers(window);
+        glfwPollEvents();
     }
     
     // free resources
